31fbfadafd04d21353ee1ba73f51955e961c8ea6
Lam Nguyen <lam.nguyen@eastgate-software.com>
2023-02-13 20:37:18 +0700

    implement sorting

--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -2,6 +2,8 @@
 using System.Reflection;
 using System.Text;
 using Entities.Models;
+using Microsoft.VisualBasic;
+using Repository.Extensions.Utility;
 
 namespace Repository.Extensions
 {
@@ -25,30 +27,9 @@ namespace Repository.Extensions
       if (string.IsNullOrWhiteSpace(orderByQueryString))
         return employees.OrderBy(e => e.Name);
 
-      var orderParams = orderByQueryString.Trim().Split(',');
-      var propertyInfos = typeof(Employee).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-      var orderQueryBuilder = new StringBuilder();
+      var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
 
-      foreach (var param in orderParams)
-      {
-        if (string.IsNullOrWhiteSpace(param))
-          continue;
-
-        var propertyFromQueryName = param.Split(" ")[0];
-        var objectProperty = propertyInfos.FirstOrDefault(pi =>
-          pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
-
-        if (objectProperty == null)
-          continue;
-
-        var direction = param.EndsWith(" desc") ? "descending" : "ascending";
-
-        orderQueryBuilder.Append($"{objectProperty.Name} {direction}, "); 
-      }
-
-      var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
-
-      return string.IsNullOrWhiteSpace(orderQuery) ? 
+      return string.IsNullOrEmpty(orderQuery) ?
         employees.OrderBy(e => e.Name) :
         employees.OrderBy(orderQuery);
     }
